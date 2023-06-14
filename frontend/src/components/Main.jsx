d055112153eba6041e004f706bd026b37acfda63
Oscar <oscar.sindemark@gmail.com>
2023-06-14 14:39:05 -0700

    tried to change stuff in Main.jsx for the data.response but changed it all back

--- frontend/src/components/Main.jsx
+++ frontend/src/components/Main.jsx
@@ -8,8 +8,8 @@ import user from "reducers/user";
 const Main = () => {
     const thoughtItems = useSelector((store) => store.thoughts.items);
     const dispatch = useDispatch();
-    const accessToken = useSelector(store => store.user.accessToken);
-    const username = useSelector(store => store.user.username);
+    const accessToken = useSelector((store) => store.user.accessToken);
+    const username = useSelector((store) => store.user.username);
     const navigate = useNavigate();
     useEffect( ()=>{
         if(!accessToken) {
@@ -32,17 +32,18 @@ const Main = () => {
                     dispatch(thoughts.actions.setError(null));
                     dispatch(thoughts.actions.setItems(data.response));
                 } else {
-                    dispatch(thoughts.actions.setError(response));
+                    dispatch(thoughts.actions.setError(data.response));
                     dispatch(thoughts.actions.setItems([]));
                 }
             })
-    })
+    }, [accessToken, dispatch])
 
     const onLogoutButtonClick = () => {
         dispatch(user.actions.setAccessToken(null));
         dispatch(user.actions.setUsername(null));
         dispatch(user.actions.setUserId(null));
         dispatch(user.actions.setError(null));
+        dispatch(thoughts.actions.setItems([]));
     }
     return(
         <>
